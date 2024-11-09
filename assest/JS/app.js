fc8d69c9a1ff7b4508ac4a67037f30b308c54aa9
Shahrukh Shahid <engr.shahrukh786@gmail.com>
2024-11-09 15:31:00 +0500

    Update app.js

--- assest/JS/app.js
+++ assest/JS/app.js
@@ -19,8 +19,8 @@ function dataGet() {
     data['laptop'] = document.getElementById('laptop').value;
     console.log(data)
 
-    data['country'] = document.getElementById('country').value;
-    data['city'] = document.getElementById('city').value;
+    data['country'] = document.getElementById('country').value = '';
+    data['city'] = document.getElementById('city').value = '';
     document.getElementById('course').value = '';
     document.getElementById('computer').value = '';
     document.getElementById('fullName').value = '';
